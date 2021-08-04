41c70c3b2a166f428830ea06f3bf43a4d4688f9a
Djina <djina.djoric98@gmail.com>
2021-08-04 23:43:37 +0200

    login form validations

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  validationUserMessage = {
+    email: [
+      {type: 'required', message:'Unesite Vašu email adresu'},
+      {type: 'pattern', message: 'Email adresa nije ispravna. Pokušajte ponovo'}
+    ],
+    password: [
+      {type: 'required', message:'Unesite Vašu lozinku'},
+      {type: 'minlength', message: 'Lozinka mora da ima minimum 7 karaktera'}
+    ]
+  };
+
+  validationFormUser: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validationFormUser = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(7)
+      ])),
+    });
+  }
+
+  loginUser(value){
+    console.log('Ulogovan');
   }
 
 }
--- src/app/auth/welcome/welcome.page.ts
+++ src/app/auth/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  constructor(private nav: NavController) { }
 
   ngOnInit() {
+  }
+
+  goToLoginPage(){
+    this.nav.navigateForward(['login']);
+  }
+
+  goToRegisterPage(){
+    this.nav.navigateForward(['signup']);
   }
 
 }
